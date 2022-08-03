c57547b527df0ab28534ec17b1f7390ae5c5701a
Francisco Javier Saldivar Rubio <fsaldivar.dev@gmail.com>
2022-08-02 21:47:45 -0500

    resolve swiftlint.

--- Sources/annotations/EmailAnnotation.swift
+++ Sources/annotations/EmailAnnotation.swift
@@ -9,7 +9,6 @@ import Foundation
 @propertyWrapper
 public struct Email<Value: StringProtocol>: ASAnnotationWrapped {
     private var value: Value?
-    
     public init(wrappedValue value: Value? = nil) {
         self.wrappedValue = value
     }
@@ -18,7 +17,7 @@ public struct Email<Value: StringProtocol>: ASAnnotationWrapped {
         get { return self.value }
         set { self.value = self.tranformValue(wrappedValue: newValue) }
     }
-    
+
     public func tranformValue<T>(wrappedValue: T?) -> T? {
         guard let stringValue = wrappedValue as? Value else {
             return wrappedValue
