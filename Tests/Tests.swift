429c7ad2439fac2ed9a97731b6b652ff6fed090d
Francisco Javier Saldivar Rubio <fsaldivar.dev@gmail.com>
2022-08-02 21:12:09 -0500

    trigger swiftlint

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -7,12 +7,10 @@
 
 import XCTest
 
-class Tests: XCTestCase {
-
+class Tests: XCTestCase{
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
